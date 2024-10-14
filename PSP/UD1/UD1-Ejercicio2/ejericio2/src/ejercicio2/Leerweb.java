3eb0a989c79813541d21d97ee264e01876e1147d
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-10-14 09:03:32 +0200

    Terminar Primer apartado

--- PSP/UD1/UD1-Ejercicio2/ejericio2/src/ejercicio2/Leerweb.java
+++ PSP/UD1/UD1-Ejercicio2/ejericio2/src/ejercicio2/Leerweb.java
@@ -3,5 +3,16 @@ package ejercicio2;
 import java.io.*;
 
 public class Leerweb {
-
+    public static void main(String[] args) {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        String webs;
+        try {
+            System.out.println("Introduzca cadena a enviar:");
+            webs = br.readLine();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
