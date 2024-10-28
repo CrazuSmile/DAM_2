f57150a541ee81c5233feb8ddce1d28b99be533d
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-10-28 09:27:42 +0100

    Primer avance

--- PSP/UD2/ejercicio1/GrupoHilos.java
+++ PSP/UD2/ejercicio1/GrupoHilos.java
@@ -1,3 +1,27 @@
-public class GrupoHilos extends Thread {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 
+public class GrupoHilos extends Thread {
+    public void run() {
+        File menu = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            menu = new File("menu.txt");
+            fr = new FileReader(menu);
+            br = new BufferedReader(fr);
+
+            String linea;
+            String regex = "-";
+            String[] array;
+
+            while ((linea = br.readLine()) != null) {
+                array = linea.split(regex);
+            }
+        } catch (Exception e) {
+            e.getLocalizedMessage();
+        }
+
+    }
 }
--- PSP/UD2/ejercicio1/Menu.java
+++ PSP/UD2/ejercicio1/Menu.java
@@ -1,3 +1,14 @@
 public class Menu {
+    public static void main(String[] args) {
+        ThreadGroup grupo = new ThreadGroup("Menu");
 
+        GrupoHilos g = new GrupoHilos();
+        Thread h1 = new Thread(grupo, g, "Primeros");
+        Thread h2 = new Thread(grupo, g, "Segundos");
+        Thread h3 = new Thread(grupo, g, "Postres");
+
+        h1.start();
+        h2.start();
+        h3.start();
+    }
 }
